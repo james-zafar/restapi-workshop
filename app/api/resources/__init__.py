1778e94f6c907ddd3792b77674e5f7b5fa6f542c
jameszafar <jzafar81@gmail.com>
2023-05-27 22:28:58 +0100

    Add resources

---
+++ app/api/resources/__init__.py
@@ -0,0 +1,9 @@
+from app.api.resources.results import ResultItem
+from app.api.resources.model import Model
+from app.api.resources.status import Status
+
+__all__ = [
+    'Model',
+    'ResultItem',
+    'Status'
+]
